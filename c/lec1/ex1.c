d65c699327d5f2208e16854feae2adc4fc27179a
ahmed elshebrawy <aelshebrawy@gmail.com>
2022-02-16 01:23:54 +0200

    first commit

---
+++ c/lec1/ex1.c
@@ -0,0 +1,16 @@
+/*
+ *  ============================================================================
+ * ex1.c
+ *
+ *  Created on: ahmed elshabrawy
+ *      Author: aelsh
+ email:aelshebrawy@gmail.com
+  ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	printf("welcome ahmed");
+
+}
